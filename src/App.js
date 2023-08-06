74234060dd613cf1a59c66614d0366fdfb44047b
amrita jassal <amrita.singh@trainingmug.com>
2023-08-06 16:10:02 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'swiper/swiper.min.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+import Error from './pages/Error';
+
+import './App.css';
+import Home from './pages/home/Home';
+import Navbar from './component/navbar/Navbar';
+import Footer from './component/footer/Footer';
+import { ConText } from './context/DataContext';
+import Product from './pages/product/Product';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ConText>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/product/:pid" component={Product} />
+
+         
+          <Route component={Error} />
+        </Switch>
+        <Footer />
+      </ConText>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}>
     <App />
-  </React.StrictMode>
+  </AlertProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
